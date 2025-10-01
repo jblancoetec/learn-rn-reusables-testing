1cae78fb0b1c23d6d6ce7c198a6cf77cfdd6d302
Joaquin Blanco <blancojoaquin77@gmail.com>
2025-09-30 23:05:24 -0300

    nueva funcionalidad: ahora es posible obtener localizacion del dispositivo

---
+++ __tests__/localizacion/obtener_localizacion.test.tsx
@@ -0,0 +1,37 @@
+import usarLocalizacion from '@/src/localizacion';
+import { renderHook, waitFor } from '@testing-library/react-native';
+
+jest.mock('expo-location', () => {
+  const obtenerLocalizacionActualFalsa = jest.fn(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    return { coords: { latitude: 10, longitude: 20 } };
+  });
+
+  const solicitarPermisosDeLocalizacionFalsa = jest.fn(async () => {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    return { status: 'granted' };
+  });
+
+  return {
+    requestForegroundPermissionsAsync: solicitarPermisosDeLocalizacionFalsa,
+    getCurrentPositionAsync: obtenerLocalizacionActualFalsa,
+  };
+});
+
+describe('Yo como usuario quiero visualizar los datos del clima de la fecha para saber vestirme ', () => {
+  test('El primer dato a visualizar en la aplicación debe ser el nombre de la ciudad: Quilmes', async () => {
+    const resultado = renderHook(() => usarLocalizacion());
+
+    expect(resultado.result.current.coordenadas()).toEqual({
+      latitud: 0,
+      longitud: 0,
+    });
+
+    await waitFor(() => {
+      expect(resultado.result.current.coordenadas()).toEqual({
+        latitud: 10,
+        longitud: 20,
+      });
+    });
+  });
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,21 @@
 import LayoutParaLaPantallaPrincipalDelClima from '@/src/clima/layouts';
 import NavParaDesplazarseEntreDias from '@/src/dias';
-import { useFechas } from '@/src/dias/hooks';
+import { usarFechas } from '@/src/dias/hooks';
+import usarLocalizacion from '@/src/localizacion';
+import { View, Text } from 'react-native';
 
 const PantallaInicialParaElClima = () => {
-  const { fechas } = useFechas();
+  const { fechas } = usarFechas();
+  const { coordenadas, coordenadasComoTexto, coordenadasDisponibles } = usarLocalizacion();
+
   return (
     <LayoutParaLaPantallaPrincipalDelClima>
       <NavParaDesplazarseEntreDias {...fechas()} />
+      <View>
+        {coordenadasDisponibles() && (
+          <Text className="text-6xl">Coordenadas: {coordenadasComoTexto()}</Text>
+        )}
+      </View>
     </LayoutParaLaPantallaPrincipalDelClima>
   );
 };
